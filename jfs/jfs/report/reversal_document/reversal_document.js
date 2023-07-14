9d2c6b073985f53cbd596b52ff8d98a05a1b77bc
shyam-kumar8848 <shyamkumar@8848digital.com>
2023-07-14 16:42:18 +0530

    Reversal Document report

---
+++ jfs/jfs/report/reversal_document/reversal_document.js
@@ -0,0 +1,298 @@
+frappe.query_reports["Reversal Document"] = {
+	filters: [
+	  {
+		fieldname: "company",
+		label: "Company",
+		fieldtype: "Link",
+		options: "Company",
+	  },
+	  {
+		fieldname: "supplier",
+		label: "Supplier",
+		fieldtype: "Link",
+		options: "Supplier",
+	  },
+	  {
+		fieldname: "posting_date",
+		label: "From Date",
+		fieldtype: "Date",
+	  },
+	  {
+		fieldname: "posting_date",
+		label: "To Date",
+		fieldtype: "Date",
+	  },
+	  {
+		fieldname: "reversal_percentage",
+		label: "Reversal Percentage",
+		fieldtype: "Float",
+		"default":50
+	  },
+	],
+	onload: function (report) {
+	  var newButton = report.page.add_inner_button(
+		__("Make Reversal"),
+		function () {
+		  // Custom button functionality here
+		  frappe.call({
+			method: "jfs.jfs.report.reversal_document.reversal_document.get_data",
+		args: {
+		'filters': {
+		  'company': frappe.query_report.get_filter_value('company'),
+		  'supplier': frappe.query_report.get_filter_value('supplier'),
+		  'posting_date': frappe.query_report.get_filter_value('posting_date'),
+		  'reversal_percentage': frappe.query_report.get_filter_value('reversal_percentage')
+		},
+	  },
+			callback: function (r) {
+			  console.log(r.message);
+			  if (r.message == "0") {
+				frappe.msgprint("No Purchase  found");
+			  } else {
+				dialog.hide();
+				create_dialog_employee(r.message);
+			  }
+			},
+		  });
+		  // Open a popup dialog
+		  var dialog = new frappe.ui.Dialog({
+			title: __("Make Reversal"),
+			primary_action: function () {
+			  // Perform the reversal action using the entered values
+			  var reversalPercentage = dialog.get_value("reversal_percentage");
+			  // Code for reversal action goes here
+			  // Close the dialog
+			  dialog.hide();
+			},
+			primary_action_label: __("Submit"),
+		  });
+		  dialog.show();
+		}
+	  );
+	},
+  };
+  function create_dialog_employee(emp_data) {
+	var data = {};
+	let d = new frappe.ui.Dialog({
+	  title: "Reversal Document",
+	  fields: [
+		{
+		  label: "Purchase Invoice",
+		  fieldname: "purchase_invoice",
+		  fieldtype: "Table",
+		  cannot_add_rows: true,
+		  in_place_edit: false,
+		  data: [],
+		  fields: [
+			{
+			  fieldname: "name",
+			  columns: 2,
+			  fieldtype: "Link",
+			  option: "Purchase Invoice",
+			  in_list_view: 1,
+			  label: "Purchase Invoice",
+			},
+			{
+			  fieldname: "posting_date",
+			  columns: 2,
+			  fieldtype: "Date",
+			  in_list_view: 1,
+			  label: "Posting Date",
+			},
+			{
+			  fieldname: "bill_date",
+			  columns: 2,
+			  fieldtype: "Date",
+			  in_list_view: 1,
+			  label: "Supplier Invoice Date",
+			},
+			{
+			  fieldname: "gap_days",
+			  columns: 2,
+			  fieldtype: "int",
+			  in_list_view: 1,
+			  label: "Age(Days)",
+			},
+			{
+			  fieldname: "billing_address",
+			  columns: 2,
+			  fieldtype: "Link",
+			  in_list_view: 1,
+			  label: "Company Address",
+			},
+			{
+			  fieldname: "expense_account",
+			  columns: 2,
+			  fieldtype: "Link",
+			  option: "Account",
+			  in_list_view: 1,
+			  label: "Expense Account",
+			},
+			{
+			  fieldname: "account_head",
+			  columns: 2,
+			  fieldtype: "Link",
+			  in_list_view: 1,
+			  label: "Account Name",
+			},
+			{
+			  fieldname: "cgst_account_head",
+			  columns: 2,
+			  fieldtype: "Link",
+			  option: "Account",
+			  in_list_view: 1,
+			  label: "CGST Account Head ",
+			},
+			{
+			  fieldname: "sgst_account_head",
+			  columns: 2,
+			  fieldtype: "Link",
+			  in_list_view: 1,
+			  label: "SGST Account Head",
+			},
+			{
+			  fieldname: "cgst",
+			  columns: 1,
+			  fieldtype: "Data",
+			  in_list_view: 1,
+			  label: "CGST AMT",
+			},
+			{
+			  fieldname: "sgst",
+			  columns: 1,
+			  fieldtype: "Data",
+			  in_list_view: 1,
+			  label: "SGST AMT",
+			},
+			{
+			  fieldname: "igst",
+			  columns: 1,
+			  fieldtype: "Data",
+			  in_list_view: 1,
+			  label: "IGST AMT",
+			},
+			{
+			  fieldname: "reversal_percentage",
+			  columns: 1,
+			  fieldtype: "Data",
+			  in_list_view: 1,
+			  label: "Reversal Percentage",
+			},
+			{
+			  fieldname: "rcgst",
+			  columns: 1,
+			  fieldtype: "Data",
+			  in_list_view: 1,
+			  label: "Reversal CGST Amt",
+			},
+			{
+			  fieldname: "rsgst",
+			  columns: 1,
+			  fieldtype: "Data",
+			  in_list_view: 1,
+			  label: "Reversal SGST Amt",
+			},
+			{
+			  fieldname: "rigst",
+			  columns: 1,
+			  fieldtype: "Data",
+			  in_list_view: 1,
+			  label: "Reversal IGST Amt",
+			},
+		  ],
+		},
+	  ],
+	  primary_action_label: "Submit",
+	  primary_action(values) {
+		let selected_items = cur_dialog.fields_dict.purchase_invoice.grid.get_selected_children();
+	  console.log(selected_items.length)
+	  let date = new Date().toLocaleDateString("de-DE");
+	  console.log(date)
+		if (selected_items.length > 0) {
+		  let arraya = [];
+		  for (let item of selected_items) {
+			if (item.igst) {
+			  arraya = [
+				{
+				  account: item.account_head,
+				  debit_in_account_currency: 0,
+				  credit_in_account_currency: item.igst,
+				},
+				{
+				  account: item.expense_account,
+				  debit_in_account_currency: item.igst,
+				  credit_in_account_currency: 0,
+				},
+			  ];
+			} else {
+			  arraya = [
+				{
+				  account: item.cgst_account_head,
+				  debit_in_account_currency: 0,
+				  credit_in_account_currency: item.cgst,
+				},
+				{
+				  account: item.sgst_account_head,
+				  debit_in_account_currency: 0,
+				  credit_in_account_currency: item.sgst,
+				},
+				{
+				  account: item.expense_account,
+				  debit_in_account_currency: item.cgst * 2,
+				  credit_in_account_currency: 0,
+				},
+			  ];
+			}
+			frappe.db
+			  .insert({
+				doctype: "Journal Entry",
+				posting_date: frappe.datetime.nowdate(),
+				voucher_type: "Reversal Of ITC",
+				reversal_type: "Others",
+				company_address: item.billing_address,
+				accounts: arraya,
+			  })
+			  .then((doc) => {
+				console.log(doc);
+			  });
+		  }
+		} else {
+		  frappe.msgprint("Please Select The checkboxes");
+		}
+		// let data_vls=values['employee_name']
+		// data_vls.forEach((i) => {
+		//   var a = frappe.model.add_child(cur_frm.doc, "Journal Entry", "accounts");
+		//    a.employee = i.employee;
+		// })
+		// refresh_field("project_employee_allocation");
+		d.hide();
+	  },
+	});
+	setTimeout(function () {
+	  $.each(emp_data, function (i, d) {
+		cur_dialog.fields_dict.purchase_invoice.df.data.push({
+		  name: d.name,
+		  posting_date:d.posting_date,
+		  bill_date:d.bill_date,
+		  gap_days:d.gap_days,
+		  billing_address: d.billing_address,
+		  account_head: d.account_head,
+		  expense_account: d.expense_account,
+		  cgst_account_head: d.cgst_account_head,
+		  sgst_account_head: d.sgst_account_head,
+		  cgst: d.cgst,
+		  sgst: d.sgst,
+		  igst: d.igst,
+		  reversal_percentage: d.reversal_percentage,
+		  rcgst: d.rcgst,
+		  rsgst: d.rsgst,
+		  rigst: d.rigst,
+		});
+		data = cur_dialog.fields_dict.purchase_invoice.df.data;
+		cur_dialog.fields_dict.purchase_invoice.grid.refresh();
+	  });
+	}, 1000);
+	d.show();
+	d.$wrapper.find(".modal-dialog").css("max-width", "60%");
+  }
+  
---
+++ jfs/jfs/report/reversal_document/reversal_document.py
@@ -0,0 +1,236 @@
+import datetime
+import frappe
+def execute(filters=None):
+    conditions,select_fields= get_conditions(filters)
+    columns, data = [], []
+    columns = [ {
+                "label": "Invoice No",
+                "fieldname": "name",
+                "fieldtype": "Link",
+                "options": "Purchase Invoice",
+                "width": 120
+                },
+                {
+                "label": "Date",
+                "fieldname": "posting_date",
+                "fieldtype": "Link",
+                "options": "Purchase Invoice",
+                "width": 120
+                },
+                {
+                "label": "Company Address",
+                "fieldname": "billing_address",
+                "fieldtype": "Link",
+                "options": "Purchase Invoice",
+                "width": 120
+                },
+                {
+                "label": "Supplier",
+                "fieldname": "supplier",
+                "fieldtype": "Link",
+                "options": "Supplier",
+                "width": 120
+                },
+                {
+                "label": "Supplier Name.",
+                "fieldname": "supplier_name",
+                "fieldtype": "Data",
+                "width": 120
+                },
+                {
+                "label": "Supplier Invoice Date",
+                "fieldname": "bill_date",
+                "fieldtype": "Data",
+                "width": 120
+                },
+                {
+                "label": "Age(Days)",
+                "fieldname": "gap_days",
+                "fieldtype": "Int",
+                "width": 100
+                },
+                {
+                "label": "Expense Gl Account",
+                "fieldname": "expense_account",
+                "fieldtype": "Link",
+                "options":"Account",
+                "width": 120
+                },
+                {
+                "label": "Total",
+                "fieldname": "base_total",
+                "fieldtype": "Data",
+                "width": 120
+                },
+                 {
+                "label": "Account Name",
+                "fieldname": "account_head",
+                "fieldtype": "Data",
+                "width": 120
+                },
+                 {
+                "label": " CGST Account Name",
+                "fieldname": "cgst_account_head",
+                "fieldtype": "Data",
+                "width": 120
+                },
+                 {
+                "label": " SGST Account Name",
+                "fieldname": "sgst_account_head",
+                "fieldtype": "Data",
+                "width": 120
+                },
+                 {
+                "label": " IGST Account Name",
+                "fieldname": "igst_account_head",
+                "fieldtype": "Data",
+                "width": 120
+                },
+                {
+                "label": "CGST AMT",
+                "fieldname": "cgst",
+                "fieldtype": "Float",
+                "width": 120
+                },
+                {
+                "label": "SGST AMT",
+                "fieldname": "sgst",
+                "fieldtype": "Float",
+                "width": 120
+                },
+                {
+                "label": "IGST AMT",
+                "fieldname": "igst",
+                "fieldtype": "Data",
+                "width": 120
+                },
+                {
+                 "label": "Reversal Percentage",
+                "fieldname": "reversal_percentage",
+                "fieldtype": "Data",
+                "width": 120
+                },
+                {
+                "label": "Reversal CGST Amt",
+                "fieldname": "rcgst",
+                "fieldtype": "Data",
+                "width": 120
+                },
+                {
+                "label": "Reversal SGST Amt",
+                "fieldname": "rsgst",
+                "fieldtype": "Data",
+                "width": 120
+                },
+                {
+                "label": "Reversal IGST Amt",
+                "fieldname": "rigst",
+                "fieldtype": "Data",
+                "width": 120
+                },
+]
+    data = frappe.db.sql(f"""
+SELECT
+    pi.`name`,
+    pi.`posting_date`,
+    pi.`billing_address`,
+    pi.`supplier`,
+    pi.`supplier_name`,
+    pi.`bill_date` AS bill_date,
+    pii.`expense_account`,
+    pi.`base_total`,
+    ptc.`account_head` AS account_head ,
+    cgst.account_head AS cgst_account_head,
+    sgst.account_head AS sgst_account_head,
+    igst.account_head AS igst_account_head,
+    cgst.tax_amount AS cgst,
+    sgst.tax_amount AS sgst,
+    igst.tax_amount AS igst
+    {select_fields} ,
+    cgst.tax_amount * COALESCE({filters.reversal_percentage} , pi.reversal_percentage) /100 AS rcgst,
+    sgst.tax_amount * COALESCE({filters.reversal_percentage}   , pi.reversal_percentage)/100 AS rsgst,
+    igst.tax_amount * COALESCE({filters.reversal_percentage} , pi.reversal_percentage)/100 AS rigst
+FROM
+    `tabPurchase Invoice` AS pi
+    LEFT JOIN `tabPurchase Invoice Item` AS pii ON pii.parent = pi.name
+    LEFT JOIN `tabPurchase Taxes and Charges` AS ptc ON ptc.parent = pi.name
+    LEFT JOIN `tabPurchase Taxes and Charges` AS cgst ON cgst.parent = pi.name AND cgst.account_head LIKE '%%CGST%%'
+    LEFT JOIN `tabPurchase Taxes and Charges` AS sgst ON sgst.parent = pi.name AND sgst.account_head LIKE '%%SGST%%'
+    LEFT JOIN `tabPurchase Taxes and Charges` AS igst ON igst.parent = pi.name AND igst.account_head LIKE '%%IGST%%'
+WHERE
+    (cgst.account_head LIKE '%%CGST%%' OR sgst.account_head LIKE '%%SGST%%' OR igst.account_head LIKE '%%IGST%%')
+    {conditions}
+GROUP BY
+    pi.`name`
+ORDER BY
+    pi.`name`
+ """, as_dict=True)
+    for item in data:
+        invoice_date = item.get("bill_date")
+        if invoice_date:
+            present_date = datetime.datetime.now().date()
+            gap_days = (present_date - invoice_date).days
+            item["gap_days"] = gap_days
+    return columns, data
+@frappe.whitelist()
+def get_data():
+    data = frappe.db.sql(f"""
+SELECT
+    pi.`name`,
+    pi.`posting_date`,
+    pi.`billing_address`,
+    pi.`supplier`,
+    pi.`supplier_name`,
+    pi.bill_date AS bill_date,
+    pii.`expense_account`,
+    pi.`base_total`,
+    ptc.`account_head` AS account_head ,
+    cgst.account_head AS cgst_account_head,
+    sgst.account_head AS sgst_account_head,
+    igst.account_head AS igst_account_head,
+    cgst.tax_amount AS cgst,
+    sgst.tax_amount AS sgst,
+    igst.tax_amount AS igst,
+    50 AS reversal_percentage,
+    cgst.tax_amount/2 AS rcgst ,
+    sgst.tax_amount/2 AS rsgst ,
+    igst.tax_amount/2 AS rigst
+FROM
+    `tabPurchase Invoice` AS pi
+   LEFT JOIN `tabPurchase Invoice Item` AS pii ON pii.parent = pi.name
+    LEFT JOIN `tabPurchase Taxes and Charges` AS ptc ON ptc.parent = pi.name
+    LEFT JOIN `tabPurchase Taxes and Charges` AS cgst ON cgst.parent = pi.name AND cgst.account_head LIKE '%%CGST%%'
+    LEFT JOIN `tabPurchase Taxes and Charges` AS sgst ON sgst.parent = pi.name AND sgst.account_head LIKE '%%SGST%%'
+    LEFT JOIN `tabPurchase Taxes and Charges` AS igst ON igst.parent = pi.name AND igst.account_head LIKE '%%IGST%%'
+WHERE
+    (ptc.account_head LIKE '%%CGST%%' OR ptc.account_head LIKE '%%SGST%%' OR ptc.account_head LIKE '%%IGST%%')
+      AND pi.docstatus = 1
+GROUP BY
+    pi.`name`
+ORDER BY
+    pi.`name`
+""",
+as_dict=1)
+    for item in data:
+        invoice_date = item.get("bill_date")
+        if invoice_date:
+            present_date = datetime.datetime.now().date()
+            gap_days = (present_date - invoice_date).days
+            item["gap_days"] = gap_days
+    return  data
+def get_conditions(filters):
+    conditions = ""
+    if filters.company:
+        conditions +=f' AND pi.company ="{filters.get("company")}" '
+    if filters.supplier:
+        conditions += f' AND pi.supplier = "{filters.get("supplier")}"'
+    if filters.posting_date:
+        conditions += f' AND pi.posting_date = "{filters.get("posting_date")}"'
+    if filters.reversal_percentage:
+        select_fields = f", {filters.reversal_percentage} AS `reversal_percentage`"
+    else:
+        select_fields = ", pi.reversal_percentage AS `reversal_percentage`"
+    if filters.present_date:
+        conditions += f' AND pi.posting_date = "{filters.get("posting_date")}"'
+    print(select_fields)
+    return conditions,select_fields
